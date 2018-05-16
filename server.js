46fdd68f3d22a3eb6da9eb36c54095512bc5f467
Joie Cosby <jc283108@gmail.com>
2018-05-16 14:53:24 -0600

    stuff

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = process.env.PORT || "3000"
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.listen("3000", function() {console.log("port 3k")});
+app.listen(port, function() {console.log("port 3k")});
