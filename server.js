1c2f0652d75723f1750ade6a483259288ba29a12
Joie Cosby <jc283108@gmail.com>
2018-05-16 14:50:05 -0600

    stuff

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen("3000", function() {console.log("port 3k")});
